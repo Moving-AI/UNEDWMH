d4ee9e792da6444dbbf3db8792440078bb35e21d
Gonzalo <gonzalo.izaguirre@bosonit.com>
2019-05-16 17:22:47 +0200

    sample_best_images function added. Generates 100 images and saves the 10 best according to the discriminator mark

--- funciones_wgan.py
+++ funciones_wgan.py
@@ -122,3 +122,35 @@ def generate_images(generator_model, output_dir, epoch, n_images, method='FLAIR'
     fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
     fig.savefig(f'{output_dir}{method}/epoch_{epoch}.png')
     plt.close(fig)
+
+def sample_best_images(generator_model, output_dir, epoch='No', n_images = 10, n_images_total = 100):
+    """Coger las mejores imágenes.
+    Cogemos por defecto 100 imágenes del generador (controlandolo con n_images_total)
+    Seleccionamos las n_images mejores y las guardamos en un archivo. Guardamos por separado las
+    FLAIR y las T1.
+    """
+    images = generator_model.predict(np.random.rand(n_images_total, 128))
+    images_mark = discriminator.predict(images).reshape((n_images_total))
+    order = np.argsort(-images_mark)[:n_images]
+    
+    cols = 2
+    images_final = images[order,...]
+    print(images.shape)
+    
+    figFLAIR = plt.figure()
+    
+    for n, image in enumerate(images_final):
+        a = figFLAIR.add_subplot(np.ceil(n_images / float(cols)), cols, n + 1)
+        plt.imshow(image[..., 1], cmap='gray')
+        
+    figFLAIR.set_size_inches(np.array(figFLAIR.get_size_inches()) * n_images)
+    figFLAIR.savefig(f'{output_dir}FLAIR/epoch_{epoch}.png')
+    plt.close(figFLAIR)
+    figT1 = plt.figure()
+    for n, image in enumerate(images_final):
+        a = figT1.add_subplot(np.ceil(n_images / float(cols)), cols, n + 1)
+        plt.imshow(image[..., 0], cmap='gray')
+    figT1.set_size_inches(np.array(figT1.get_size_inches()) * n_images)
+    print(f'{output_dir}T1/epoch_{epoch}.png')
+    figT1.savefig(f'{output_dir}T1/epoch_{epoch}.png')
+    plt.close(figT1)
